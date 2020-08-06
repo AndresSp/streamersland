61017908bc4f307d2d490af0fc46b9a2164b954c
AndresSp <andrestunonsp@gmail.com>
2020-08-06 00:12:53 +0000

    Deploying to gh-pages from  @ 3b79d758c71e6f69bf3183dc69fa2dffb11693fd 🚀

---
+++ precache-manifest.31ac6ed3bfa47d2673a625e2d4cab246.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9385517590ea37c41f2f694898a92b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2baffcc2044936163245",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c4484296f1e66942639a",
+    "url": "/static/js/2.c9ef0887.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c9ef0887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2baffcc2044936163245",
+    "url": "/static/js/main.799bc43c.chunk.js"
+  },
+  {
+    "revision": "d14b0d771550bcd0551f",
+    "url": "/static/js/runtime-main.66a97362.js"
+  },
+  {
+    "revision": "3ef5213ed0119333508eaf2d09cc5545",
+    "url": "/static/media/cube.3ef5213e.svg"
+  },
+  {
+    "revision": "14939a30c7d05bb80493f07544d1c745",
+    "url": "/static/media/maple_cursor.14939a30.svg"
+  },
+  {
+    "revision": "a77f67510c98c2e1575e6fb702a64fc8",
+    "url": "/static/media/maple_cursor.a77f6751.cur"
+  },
+  {
+    "revision": "74dc8598bd397fe694e4563672d0afa8",
+    "url": "/static/media/mcwp.74dc8598.jpg"
+  },
+  {
+    "revision": "1764c648e2d8d0dfcc7675439108bc67",
+    "url": "/static/media/menu.1764c648.mp3"
+  },
+  {
+    "revision": "27471733ba0c3d51e545037d7aa9c7ca",
+    "url": "/static/media/smashTemplate.27471733.png"
+  },
+  {
+    "revision": "fe44f7358eaf358444a0e63a2eecee1a",
+    "url": "/static/media/streamersland.fe44f735.svg"
+  }
+]);
